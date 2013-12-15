7ac287a74543196d2ac38caf38b2764a407dc81c
Gilles Roustan <contact@gb-prod.fr>
2013-12-15 13:55:26 +0100

    Resolution Angular SocketIO Live coding

--- live-angular-socket-io/server.js
+++ live-angular-socket-io/server.js
@@ -6,7 +6,44 @@ httpServer.listen(1337);
 
 var io = require('socket.io').listen(httpServer);
 
+var users = {};
+var messages = new Array();
+
 io.sockets.on('connection', function(socket){
     console.log("connection");
+
+    var me;
+
+    socket.on('authenticate', function(pseudo) {
+        if ('' == pseudo) {
+            return;
+        };
+
+        console.log("authenticate : " + pseudo);
+
+        users[pseudo] = {
+            'pseudo': pseudo
+        };
+        me = pseudo;
+
+        io.sockets.emit('update.users', users);
+    });
+
+    socket.on('disconnect', function() {
+        delete users[me];
+        io.sockets.emit('update.users', users);
+    });
+
+    socket.on('new.message', function(message) {
+        messages.push({
+            'message': message,
+            'user': me
+        });
+
+        io.sockets.emit('add.message', {
+            'message': message,
+            'user': me
+        });
+    });
 });
 
